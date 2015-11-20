d20ea9f4279b8ba4e5131544266f11f1b830e0d7
Clay Cressler <clay.cressler@icloud.com>
2015-11-20 07:04:26 -0600

    New code for fitting Cat's growth and reproduction dataset with fixed fh values

--- D_dent_fitting/Growth_reproduction_trajectory_fitting_Cat.R
+++ D_dent_fitting/Growth_reproduction_trajectory_fitting_Cat.R
@@ -132,3 +132,89 @@ refine_lik %>%
                 as.data.frame -> refine_pars
 refine_pars <- arrange(refine_pars, lik)
 saveRDS(refine_pars, file="~/Dropbox/Growth_reproduction_trajectory_fitting_Cat.RDS")
+
+
+#########################################################################
+#########################################################################
+###### FITTING GROWTH AND REPRODUCTION DATA FOR UNINFECTED ANIMALS ######
+######         HOLDING F_H CONSTANT AT DIFFERENT VALUES
+######             AND FITTING ALL OTHER PARAMETERS
+#########################################################################
+#########################################################################
+source("Growth_reproduction_trajectory_fitting_functions_Cat.R")
+x <- read.csv("Cat_data/uninfected_growth_reproduction.csv")
+data <- x[1:103,1:3]
+
+
+parorder <- c("Imax","fh","g","rho","eps","V","F0","xi","q","K","km","ER","v","Lobs","Winit","Wmat")
+pars <- c(Imax=calc_Imax(10000), # fixed based on feeding data fitting and fh value
+          fh=10000, # estimated from fitting growth/reproduction data
+          g=calc_g(10000), # fixed based on feeding data fitting and fh value
+          rho=0.2, # estimated from fitting growth/reproduction data
+          eps=44.5e-9, # fixed based on measured carbon content of algae
+          V=30, # fixed based on experimental conditions
+          F0=1000000/30, # fixed based on experimental conditions
+          xi=1.8e-3, # fixed based on Hall et al. 2009 length-weight regression
+          q=3, # fixed based on Hall et al. 2009 length-weight regression
+          K=0.5, # estimated from growth/reproduction data
+          km=0.1, # estimated from growth/reproduction data
+          ER=1.51e-3, # fixed based on observed mass of a neonate
+          v=10, # fixed based on the fact that it doesn't affect the fitting
+          Lobs=0.1,
+          Winit=0.00225,
+          Wmat=0.005) # estimated from growth/reproduction data
+
+## initial guesses for the estimated parameters
+transform <- c(rep("logit",2), rep("log",2))
+box <- cbind(lower=c(rho=0, K=0, km=0.001, Lobs=0.0001),
+             upper=c(rho=1, K=1, km=10, Lobs=2))
+sobolDesign(lower=box[,'lower'],
+            upper=box[,'upper'],
+            nseq=100000) %>%
+    apply(., 1, as.list) %>%
+        lapply(., unlist) -> guesses
+
+fh_vals <- seq(100,2000,100)
+ests <- vector(mode='list', length=length(fh_vals))
+for (i in 1:length(fh_vals)) {
+    print(fh_vals[i])
+    fixpars <- pars[c("Imax","fh","g","eps","V","F0","xi","q","ER","v","Winit","Wmat")]
+    fixpars["fh"] <- fh_vals[i]
+    fixpars["Imax"] <- calc_Imax(fh_vals[i])
+    fixpars["g"] <- calc_g(fh_vals[i])
+    eventdat <- data.frame(var="F",
+                           time=1:35,
+                           value=unname(fixpars["F0"]),
+                           method=rep(c(rep("add",4),"rep"),35/5))
+    mclapply(guesses,
+             traj_match2,
+             fixpars=fixpars,
+             parorder=parorder,
+             transform=transform,
+             obsdata=data,
+             events=eventdat,
+             eval.only=TRUE,
+             mc.cores=4) %>%
+                 lapply(., function(x) x$lik) %>%
+                     unlist -> guess_lik
+    guesses[order(guess_lik)[1:2000]] -> refine
+    print(fh_vals[i])
+    mclapply(refine,
+             traj_match2,
+             fixpars=fixpars,
+             parorder=parorder,
+             transform=transform,
+             obsdata=data,
+             events=eventdat,
+             eval.only=FALSE,
+             method="Nelder-Mead",
+             mc.cores=4) -> refine_lik
+    refine_lik %>%
+        lapply(., unlist) %>%
+            unlist %>%
+                matrix(., ncol=(nrow(box)+2), byrow=TRUE, dimnames=list(1:length(refine_lik), c(rownames(box),"lik","conv"))) %>%
+                    as.data.frame -> refine_pars
+    refine_pars <- arrange(refine_pars, lik)
+    ests[[i]] <- refine_pars
+    saveRDS(ests, file="~/Dropbox/Growth_reproduction_trajectory_fitting_Cat_fixed_fh.RDS")
+}
--- D_dent_fitting/Growth_reproduction_trajectory_fitting_functions_Cat.R
+++ D_dent_fitting/Growth_reproduction_trajectory_fitting_functions_Cat.R
@@ -141,3 +141,101 @@ obj <- function(estpars, data, fixpars, parorder, transform, events) {
     ## return the negative log-likelihood (because optimization methods minimize)
     return(-lik)
 }
+
+## same code, but doesn't waste computational time calculating g and Imax, since fh is fixed
+traj_match2 <- function(estpars, fixpars, parorder, transform, obsdata, events, eval.only=FALSE, method="subplex") {
+    estpars <- par_transform(pars=estpars, transform=transform)
+    if (any(is.na(estpars)))
+        opt <- list(params=estpars,
+                    lik=NA)
+    else if (eval.only==TRUE) {
+        x <- obj2(estpars=estpars,
+                 data=obsdata,
+                 fixpars=fixpars,
+                 parorder=parorder,
+                 transform=transform,
+                 events=events)
+        opt <- list(params=estpars,
+                    lik=x)
+    }
+    else {
+        if (method=="subplex")
+            x <- subplex(par=estpars,
+                         fn=obj2,
+                         data=obsdata,
+                         fixpars=fixpars,
+                         parorder=parorder,
+                         transform=transform,
+                         events=events)
+        else
+            x <- optim(par=estpars,
+                       fn=obj2,
+                       method=method,
+                       data=obsdata,
+                       fixpars=fixpars,
+                       parorder=parorder,
+                       transform=transform,
+                       events=events,
+                       control=list(maxit=5000))
+        opt <- list(params=par_untransform(x$par,transform),
+                    lik=x$value,
+                    conv=x$convergence)
+    }
+    if (eval.only==FALSE) print(opt$lik)
+    return(opt)
+}
+## Objective function to minimize for the structured model
+obj2 <- function(estpars, data, fixpars, parorder, transform, events) {
+    y0 <- c(F=unname(fixpars["F0"]),
+            E=unname(fixpars["Winit"]/2),
+            W=unname(fixpars["Winit"]/2),
+            R=0)
+    ## Put the estimated parameters back on the natural scale
+    estpars <- par_untransform(estpars, transform)
+    ## combine the parameters to be estimated and the fixed parameters
+    ## into a single vector, with an order specified by parorder
+    pars <- c(estpars, fixpars)
+    pars[match(parorder, names(pars))] -> pars
+    if(any(names(pars)!=parorder)) stop("parameter are in the wrong order")
+    ## simulate the model at these parameters
+    try(ode(y0,
+            times=seq(0, max(data$time)),
+            func="derivs",
+            parms=pars,
+            dllname="deb_Cat",
+            initfunc="initmod",
+            events=list(data=events)
+            )) -> out
+    ## if this parameter set produces a simulation error, if it fails
+    ## to completely run, or if any of the variables take negative
+    ## values, return -Inf for the likelihood.
+    if (inherits(out, "try-error"))
+        lik <- -Inf
+    else if (max(out[,"time"]) < max(data$time))
+        lik <- -Inf
+    else if (any(is.nan(out)))
+        lik <- -Inf
+    else if (any(out < 0))
+        lik <- -Inf
+    else {
+        out <- as.data.frame(out)
+        mutate(out,
+               L=((W+E)/pars["xi"])^(1/pars["q"]),
+               R=R-R[which((E+W) < pars["Wmat"]) %>% max]) -> out
+        out$R[out$R < 0] <- 0
+        ## calculate the log-likelihood of observing these data
+        ## assuming only observation error
+        (dnorm(data$length,
+              sapply(data$times,
+                     function(t) out$L[out$time==t]),
+              unname(pars["Lobs"]),
+              log=TRUE) +
+             dpois(data$eggs,
+                   sapply(data$times,
+                          function(t) out$R[out$time==t]),
+                   log=TRUE)) %>%
+            sum(., na.rm=TRUE) -> lik
+    }
+    ## return the negative log-likelihood (because optimization methods minimize)
+    return(-lik)
+}
