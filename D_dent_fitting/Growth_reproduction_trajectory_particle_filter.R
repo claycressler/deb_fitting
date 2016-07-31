f75a9d47675e2b68df3f5ef6f493bf2766967ebe
Clay Cressler <claycressler@Clays-MacBook-Air.local>
2016-07-31 12:23:45 -0500

    Fixed particle filter code error and restarted fitting algorithm

--- D_dent_fitting/Growth_reproduction_trajectory_particle_filter.R
+++ D_dent_fitting/Growth_reproduction_trajectory_particle_filter.R
@@ -161,11 +161,21 @@ for (d in 2:25) {
 saveRDS(datasets, file="env_stoch_datasets.RDS")
 
 ## For this first set of attempts, do not attempt to estimate ER, but allow it to be fixed at the correct value.
+source("Growth_reproduction_trajectory_fitting_stochastic_functions.R")
 pf_ests <- vector(mode='list', length=25)
 tm_ests <- readRDS("Trajectory_matching_7-27.RDS")
+datasets <- readRDS("env_stoch_datasets.RDS")
+
+pars <- c(Imax=22500, fh=10000, g=1.45, rho=0.1, eps=44.5e-9, V=30, F0=1000000/30, xi=2.62e-3, q=2.4, K=0.3, km=0.15, ER=1.51e-3, v=10, Lobs=0.1, Robs=1, Ferr=10000)
+fixpars <- pars[c("Imax","g","eps","V","F0","xi","q","ER","v")]
+estpars <- pars[c("fh","rho","K","km","Lobs","Robs","Ferr")]
+transform <- c("log", rep("logit",2), rep("log",4))
+parorder <- c("Imax","fh","g","rho","eps","V","F0","xi","q","K","km","ER","v","Lobs","Robs","Ferr")
 
 for (d in 1:25) {
     print(d)
+    tic <- Sys.time()
+    print(tic)
     data <- datasets[[d]]$data
 
     estpars <- tm_ests[[d]]
@@ -188,14 +198,17 @@ for (d in 1:25) {
              eval.only=FALSE,
              type='particle_filter',
              method='Nelder-Mead',
+             Np=100,
              mc.cores=15) -> refine_lik
     refine_lik %>%
         lapply(., unlist) %>%
             unlist %>%
-                matrix(., ncol=(nrow(box)+2), byrow=TRUE, dimnames=list(1:length(refine_lik), c(rownames(box),"lik","conv"))) %>%
+                matrix(., ncol=ncol(estpars), byrow=TRUE, dimnames=list(1:length(refine_lik), colnames(estpars))) %>%
                     as.data.frame -> refine_pars
     refine_pars[order(refine_pars$lik),] -> refine_pars
     pf_ests[[d]] <- refine_pars
+    toc <- Sys.time()
+    print(tic); print(toc-tic)
     saveRDS(pf_ests, file="Particle_filter_7-30.RDS")
 }
 
